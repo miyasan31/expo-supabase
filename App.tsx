93d40d1f38e99cdb10f5d5bd3bed52ff7966883a
miyasan_0301 <miyasan.19990301@gmail.com>
2021-10-16 23:22:25 +0900

    init project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { supabase } from "./src/utils/supabaseInit";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const fetch = async () => {
+		const { data, error, status } = await supabase.from("profiles").select("*");
+		console.log(data);
+	};
+
+	useEffect(() => {
+		fetch();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>みやさんのアプリ</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
